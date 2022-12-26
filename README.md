2265b9ad5541eb077888bfccfa8e0a46857298dd
priyaan99 <priyaanyadav@gmail.com>
2022-12-26 09:33:27 +0530

    player movement wads [ Vector Addition ]

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MFGD
 Math for Game Developers. 
+
+## To Complie
+- Install [Raylib](https://github.com/raysan5/raylib) v4.2
+- $ gcc <path_to_file.c> -lraylib -lm -ogame
---
+++ src/00_player_movement_vec-add.c
@@ -0,0 +1,44 @@
+#include "raylib.h"
+#include "raymath.h"
+
+static int screen_width = 256;
+static int screen_height= 256;
+
+static Vector2 player_position;
+static int player_radius;
+
+int main() {
+    InitWindow(screen_width, screen_height, "Player Movement");
+
+    player_position = (Vector2) { screen_width*1.0f/2, screen_height*1.0f/2 };
+    player_radius = 10;
+
+    // means 200 pixel per second (must multiply with delta time)
+    const float SPEED = 200.0;
+
+    while (!WindowShouldClose()) {
+        // update
+        if (IsKeyDown(KEY_W)) player_position.y -= SPEED * GetFrameTime();
+        if (IsKeyDown(KEY_S)) player_position.y += SPEED * GetFrameTime();
+        if (IsKeyDown(KEY_A)) player_position.x -= SPEED * GetFrameTime();
+        if (IsKeyDown(KEY_D)) player_position.x += SPEED * GetFrameTime();
+
+        // stoping player from moving out of screen
+        player_position = Vector2Clamp(
+            player_position, 
+            (Vector2){0+player_radius,0+player_radius},
+            (Vector2){screen_width-player_radius, screen_height-player_radius}
+        );
+
+        // draw
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        DrawCircleV(player_position, player_radius, RED);
+        
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+}
